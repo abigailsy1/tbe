dd6490868e8dfbf67349da384fe4bf3f4ee27d94
Abby <abigailsy1@yahoo.com>
2019-01-27 00:32:41 -0800

    fixed .env file, added proxy, and updated packages

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,34 @@
 'use strict';
+const axios = require("axios");
+require('dotenv').config();
 
-// module.exports = function(server) {
-//   // Install a `/` route that returns server status
-//   var router = server.loopback.Router();
-//   router.get('/', server.loopback.status());
-//   server.use(router);
-// };
+
+module.exports = function (server) {
+    // Install a `/` route that returns server status
+    var router = server.loopback.Router();
+
+    router.get("/proxy/yelp", (req, res) => {
+        const term = req.query
+        const apiKey = process.env.API_KEY;
+        const yelpUrl = "https://api.yelp.com/v3/businesses/search";
+        console.log('term is', term)
+        axios
+            .get(yelpUrl, {
+                headers: {
+                    Authorization: "Bearer " + apiKey
+                },
+                params: {
+                    term,
+                    location: "92139"
+                }
+            })
+            .then(response => {
+                res.send(response.data); // <= send data to the client
+            })
+            .catch(err => {
+                console.log(err);
+                res.send({ err }); // <= send error
+            });
+    });
+    server.use(router);
+};
--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,23 +1,15 @@
 import axios from 'axios';
 
-const apiKey = process.env.API_KEY;
-const yelpUrl = 'https://api.yelp.com/v3/businesses/search';
-const cors = 'https://cors-anywhere.herokuapp.com/';
-const ROOT_URL = cors + yelpUrl;
 export const FETCH_DATA = "FETCH_DATA";
 
 export function fetchData(input) {
     return {
-        type: FETCH_DATA,
-        payload: axios.get(ROOT_URL, {
-            headers: {
-                Authorization:'Bearer ' + apiKey
-            },
-            params: {
-                term: input,
-                location: '92139'
-            } 
-        })
-    }
-}
+      type: FETCH_DATA,
+      payload: axios
+      .get("/proxy/yelp?term=" + input)
+      .then(resp => {
+          return resp;
+      })
+    };
+  }
 
